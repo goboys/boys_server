beccd9bfb2fef5655d3b16d3d66c5f98143a8a2a
goboys <459348471@qq.com>
2017-08-16 14:28:31 +0800

    引入boys_public

--- src/main/java/com/boys/server/impl/UserServiceImpl.java
+++ src/main/java/com/boys/server/impl/UserServiceImpl.java
@@ -3,22 +3,22 @@ package com.boys.server.impl;
 import org.springframework.stereotype.Service;
 
 import com.boys.bean.User;
-import com.boys.server.IUserService;
+import com.boys.server.iface.IUserService;
 
 @Service
 public class UserServiceImpl implements IUserService{
    
-	
-	public void createUser(User user){
-		System.out.println("save user");
-	}
-
 	public User getUserById(String userId) {
 		User user = new User();
 		user.setAge(22);
 		user.setId(1);
-		user.setName("°¢±ó,ºÜ²»´í....");
+		user.setName("SETNAME....");
 		return user;
+	}
+
+	@Override
+	public void creteUser(User user) {
+		System.out.println("save user");
 	}
 	
 }
